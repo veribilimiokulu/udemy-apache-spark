c80fedb8b11f11456ff2b3189ef809738864d605
Erkan ŞİRİN <erkansirin78@hotmail.com>
2018-09-08 13:21:25 +0300

    Scala temel 1 dersi eklendi

--- kurs_esnasi/scala/scala_temel/src/main/scala/scalaTemel/ScalaTemel.scala
+++ kurs_esnasi/scala/scala_temel/src/main/scala/scalaTemel/ScalaTemel.scala
@@ -3,5 +3,34 @@ package scalaTemel
 object ScalaTemel {
   def main(args: Array[String]): Unit = {
     println("Merhaba Scala")
+
+    println(1+1)
+
+
+    println("Merhaba"+ ", " + "Scala.")
+
+    val myVal = 10
+    //myVal =20
+
+    var myVar = 20
+    myVar = 30
+
+    println("myVar değeri: "+ myVar)
+
+    val cumle = "Benim cümlem"
+    var sayac = 0
+    cumle.foreach(x => {
+      println(x.toUpper)
+      sayac += 1
+
+    })
+    println(sayac)
+
+
+    val ikiSayiTopla = (x:Int, y:Int) => x + y
+
+    println("İki sayının fonksiyon ile toplanması: " + ikiSayiTopla(5,3))
+
+
   }
 }
