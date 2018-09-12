9e85433c8a42d62b85f67d19a6e5625880195362
Erkan ŞİRİN <erkansirin78@hotmail.com>
2018-09-12 11:41:52 +0300

    Scala temel kurs esnasına Maps eklendi

--- kurs_esnasi/scala/scala_temel/src/main/scala/scalaTemel/Maps.scala
+++ kurs_esnasi/scala/scala_temel/src/main/scala/scalaTemel/Maps.scala
@@ -3,12 +3,87 @@ package scalaTemel
 object Maps {
   def main(args: Array[String]): Unit = {
 
-    // collection, immutable, anahtar-değer,
-
-
     /****************  IMMUTEABLE MAP   *************************/
+
+    // Immutable Map oluşturma
     val ulkeBaskent = Map("Japonya"->"Tokyo",  //immutable
       "Hindistan" -> "Delhi",
       "Güney Kore" -> "Seul")
+    println("ulkeBaskent: " + ulkeBaskent)
+
+
+    // Başka bir yöntemle oluşturalım
+    val ulkeBaskent2 = Map(("ABD","Washington"), ("Fransa","Paris"))
+    println("ulkeBaskent2: " + ulkeBaskent2)
+
+
+    // if ile eğer bir ülke Map içinde var ise başkentini yazdırsın
+    var anahtar = "Almanya"
+    if(ulkeBaskent.contains(anahtar)){
+
+      println(s"Aranılan ülke $anahtar  başkenti : " + ulkeBaskent(anahtar))
+
+    }else{
+
+      println(s"Aranılan ${anahtar}, ülkeler içinde yok.")
+
+    }
+
+    // Immutable bir Map'e eleman eklenemez
+     // ulkeBaskent("Almanya") = "Berlin" // immutable.Map olduğu için hata verir
+
+
+    /****************  MUTEABLE MAP   *************************/
+
+    // Mutable Map oluşturmak
+    val ulkeBaskentMut = collection.mutable.Map("Japonya"->"Tokyo",  //muteable
+      "Hindistan" -> "Delhi",
+      "Güney Kore" -> "Seul")
+
+    // Mutable Map'e eleman ekleme
+    ulkeBaskentMut("Almanya") = "Berlin" // mutable.Map olduğu için kabul eder
+
+    // Muteable Map'a başka bir yöntemle eleman ekleme
+    ulkeBaskentMut += ("İspanya" -> "Madrid")
+
+    println(ulkeBaskentMut)
+
+
+    // if ile eğer bir ülke Map içinde var ise başkentini yazdırsın.
+    // Almanya'yı ekleyebildiğimize göre
+    if(ulkeBaskentMut.contains(anahtar)){
+      println(s"Aranılan ülke $anahtar  başkenti :" + ulkeBaskentMut(anahtar))
+    }else{
+      println(s"Aranılan ${anahtar}, ülkeler içinde yok.")
+    }
+
+
+    val ogrenciler = collection.mutable.Map(1503 -> "Salih",
+      1504 -> "Hasan",
+      1505 -> "Tuncay")
+
+    ogrenciler(1508) = "Mustafa"
+
+    println(ogrenciler)
+
+    // Map içinden anahtar ile değer çağırmak
+    println(ogrenciler(1503))
+
+    // Map içinden anahtar ile değer çağırmak
+    //println(ogrenciler(1000))  // hata verir
+
+
+    // Map ile get kullanarak olmayan bir anahtar ile değer çağırmak
+    println(ogrenciler.get(1505)) // Hata vermez None döndürür
+
+    // Sadece Map anahtarlarına ulaşmak
+    println("ulkeBaskentMut.keys: " + ulkeBaskentMut.keys)
+    println("ulkeBaskent.keys: " + ulkeBaskent.keys)
+
+
+    // Sadece Map değerlerine ulaşmak
+    println("ulkeBaskentMut.values: " + ulkeBaskentMut.values)
+    println("ulkeBaskent.values: " + ulkeBaskent.values)
+
   }
 }
---
+++ kurs_esnasi/scala/scala_temel/src/main/scala/scalaTemel/Tuples.scala
@@ -0,0 +1,5 @@
+package scalaTemel
+
+object Tuples {
+
+}
