aa40003f307401b6741e22e86f75da07aea6ec37
Erkan ŞİRİN <erkansirin78@hotmail.com>
2018-09-12 12:44:08 +0300

    Scala temel kurs esnasına Tuples eklendi

--- kurs_esnasi/scala/scala_temel/src/main/scala/scalaTemel/Tuples.scala
+++ kurs_esnasi/scala/scala_temel/src/main/scala/scalaTemel/Tuples.scala
@@ -1,5 +1,39 @@
 package scalaTemel
 
 object Tuples {
+  def main(args: Array[String]): Unit = {
+    // tuple oluşturma
+    var tupleAhmet = (1001, "Ahmet YAMAN", 86.77)
+    println(tupleAhmet)
 
+    // Tuple eleman erişim
+    println(tupleAhmet._2)
+
+
+    printf(
+      "%s'ın not ortalaması: %.2f\n",
+      tupleAhmet._2,
+      tupleAhmet._3
+    )
+
+    // tuple eleman ekleme. Immutable olduğundan eklenmez
+    //tupleAhmet += "8/A"
+
+    // Tuple yazdırmak
+    println(tupleAhmet.toString())
+
+    // tuple değerlerini değişkenlere topluca aktarmak
+    val(no, isim, notu) = tupleAhmet
+
+
+    println(no)
+
+
+    val(not1, isim1, _) = tupleAhmet
+
+    println(not1 + " - " + isim1)
+
+
+
+  }
 }
