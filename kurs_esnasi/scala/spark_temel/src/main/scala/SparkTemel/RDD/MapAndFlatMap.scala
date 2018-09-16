0b9e89646830b5c451cf41992aea498d3dea58d2
Erkan ŞİRİN <erkansirin78@hotmail.com>
2018-09-16 16:08:10 +0300

    Spark temel kurs hazırlığı scala flatmap güncellendi

---
+++ kurs_esnasi/scala/spark_temel/src/main/scala/SparkTemel/RDD/MapAndFlatMap.scala
@@ -0,0 +1,16 @@
+package SparkTemel.RDD
+
+import org.apache.log4j.{Level, Logger}
+import org.apache.spark.{SparkConf, SparkContext}
+
+object MapAndFlatMap {
+  def main(args: Array[String]): Unit = {
+    Logger.getLogger("org").setLevel(Level.ERROR)
+
+    val conf = new SparkConf().setAppName("sparkTemelRDD").setMaster("local[4]")
+    val sc = new SparkContext(conf)
+    
+
+
+  }
+}
