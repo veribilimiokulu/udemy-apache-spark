824cf611f75b61f507fe01c18f7afbe248281376
Erkan ŞİRİN <erkansirin78@hotmail.com>
2018-09-16 09:18:48 +0300

    Scala SparkTemel MapAndFlatMap'e iptal edilen tutarları hesaplama eklendi

--- kurs_hazirlik/scala/spark_temel/src/main/scala/sparkTemel/RDD/MapAndFlatMap.scala
+++ kurs_hazirlik/scala/spark_temel/src/main/scala/sparkTemel/RDD/MapAndFlatMap.scala
@@ -26,6 +26,7 @@ object MapAndFlatMap {
 
       invoiceNo+";"+total+";"+isCancelled
     })
+    println("map splitted satır sayısı: " + retailMapPriceRDD.count())
     println("retailMapPriceRDD:")
     retailMapPriceRDD.take(10).foreach(println)
 
@@ -35,15 +36,8 @@ object MapAndFlatMap {
     println("\nİptal olanların sayısı: \n" + retailMapPriceRDD.filter(x=> x.split(";")(2) == "true").count())
 
 
-    println("map splitted satır sayısı: " + retailMapPriceRDD.count())
 
 
-    println("\n \n ******************* MAPPARTITIONSWITHINDEX *****************************************")
-
-    val mapPartWithIndexRDD = retailMapPriceRDD.mapPartitionsWithIndex(
-      (id,iter) => if(id >= 0) iter else iter
-    )
-    mapPartWithIndexRDD.take(5).foreach(println)
 
     println("\n \n ******************* FLATMAP *****************************************")
     val retailFlatMapSplittedRDD = retailRDD.flatMap(x => x.split(";"))
@@ -51,11 +45,43 @@ object MapAndFlatMap {
     retailFlatMapSplittedRDD.take(10).foreach(println)
 
 
-    println()
+    println("flatMap ile her kelimeyi büyük harf yapma: ")
     val retailFlatMapToUpperRDD = retailRDD.flatMap(x => x.split(";")).map(x=>x.toUpperCase)
     println("retailFlatMapToUpperRDD")
-    retailFlatMapToUpperRDD.take(10).foreach(println)
+    retailFlatMapToUpperRDD.take(2).foreach(println)
 
+
+
+
+    println("\n \n ************* MAP İLE PAIR RDD YARATMAK ve İPTAL OLAN SATIŞLARIN TOPLAM TUTARINI HESAPLAMAK *********")
+  // Aşağıdaki bölümde iptal edilen satışların toplam tutarını bulacağız
+
+    // case class ile ihtiyacımız olan özellikleri yapısal hale getirelim
+    case class TotalCancelledPrice(isCancelled:Boolean, total:Double)
+
+    // map dönüşümü ile içinde case class(TotalCancelledPrice) olan bir rdd yaratalım
+    val retailTotalCancelledPriceRDD = retailRDD.map(line => {
+      //val invoiceNo = line.split(";")(0).takeRight(6).toInt // iptaller için c olduğundan onları atlıyoruz
+      val isCancelled = if(line.split(";")(0).startsWith("C")) true else false
+      val total = line.split(";")(3).toInt * line.split(";")(5).replace(",",".").toDouble
+
+      TotalCancelledPrice(isCancelled,total)
+    })
+    // Oluşan yeni rdd türü aşağıdadır
+    // TotalCancelledPrice: org.apache.spark.rdd.RDD[TotalPrice] = MapPartitionsRDD[3] at map at <console>:16
+    // Yeni rdd'yi görelim
+    println("\nİçinde case class taşıyan yeni TotalCancelledPrice RDD")
+    retailTotalCancelledPriceRDD.take(10).foreach(println)
+
+
+    println("\nİptal edilen toplam tutar: ")
+    retailTotalCancelledPriceRDD
+      .map(x => (x.isCancelled, x.total))  // anahtar değer oluştur. Çünkü reduceByKey bunu istiyor
+      .reduceByKey((x,y) => x+y) // true ve false olan anahtarlara göre değerleri topla
+      .filter(x => x._1 == true)  // true değer iptal edilenlerdi onları filtrele diğerleri gelmesin
+      .map(x => x._2) // Benim ihtiyacım sadece rakam. Boşuna true değerini yazma
+      .take(2) // Sonuçtan ikisini al. Bir tane bekliyoruz ama yukarıdaki filtreyi kaldırınca true ve false gelir
+      .foreach(println) // Sonucu yazdır.
 
 
 
