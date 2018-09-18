0fcd440c438c738ecb80403032e4d182b247d2a8
Erkan ŞİRİN <erkansirin78@hotmail.com>
2018-09-18 16:02:19 +0300

    Scala Spark Temel RDD kurs esnası maptransformation eklendi

--- kurs_esnasi/scala/spark_temel/src/main/scala/SparkTemel/RDD/MapTransformation.scala
+++ kurs_esnasi/scala/spark_temel/src/main/scala/SparkTemel/RDD/MapTransformation.scala
@@ -1,14 +1,38 @@
 package SparkTemel.RDD
-
 import org.apache.log4j.{Level, Logger}
 import org.apache.spark.{SparkConf, SparkContext}
-
 object MapTransformation {
   def main(args: Array[String]): Unit = {
     Logger.getLogger("org").setLevel(Level.ERROR)
 
     val conf = new SparkConf().setAppName("sparkTemelRDD").setMaster("local[4]")
     val sc = new SparkContext(conf)
+
+    // RDD okuma
+    val retailRDD = sc.textFile("C:\\Users\\toshiba\\SkyDrive\\veribilimi.co\\Datasets\\OnlineRetail.csv")
+      .filter(!_.contains("InvoiceNo"))
+
+    println(retailRDD.first())
+    case class CancelledPrice(isCancelled:Boolean, total:Double)
+    val retailTotal = retailRDD.map(x => {
+      var isCancelled:Boolean = if(x.split(";")(0).startsWith("C")) true else false
+      var quantity:Double = x.split(";")(3).toDouble
+      var price:Double = x.split(";")(5).replace(",",".").toDouble
+
+      var total:Double = quantity*price
+      CancelledPrice(isCancelled, total)
+    })
+
+    retailTotal.take(5).foreach(println)
+
+    println("\nİptal edilen toplam tutar: ")
+    retailTotal.map(x => (x.isCancelled, x.total))
+      .reduceByKey((x,y) => x + y)
+      .filter(x => x._1 == true)
+      .map(x => x._2)
+      .take(5)
+      .foreach(println)
+
 
 
 
